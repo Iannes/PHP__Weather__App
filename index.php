43e50440cb3cd1f2589f380fc83cb2764a20f174
Yannis Spyrou <js15js@gmail.com>
2017-02-04 19:27:01 +0000

    Update index.php

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ require('simple_html_dom.php');
 <head>
 	<title>Weather App</title>
 
-	  <!-- Required meta tags always come first -->
+	  <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
