df4f1698bfcd5d9c79b80e6f5047e4e4d90c7bf1
Yannis Spyrou <js15js@gmail.com>
2017-09-04 19:15:54 +0100

    Add weather icon

--- config.php
+++ config.php
@@ -8,34 +8,36 @@ $key ='__KEY__';
     
 /* === Get url ==== */
 $urlWithCity = 'http://api.openweathermap.org/data/2.5/weather?q='.$city.$key;
-$png ='icon.png';
+
 $imageUrl = 'http://openweathermap.org/img/w/'.$png;
     
 //    =========================================================
 // 						Functions
 //    =========================================================
 
-function curl($url) {
-	#  HTTP request
-	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_URL, $url);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-	$data = curl_exec($ch);
-	curl_close($ch);
-	return $data;
+function curl($url)
+{
+    #  HTTP request
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+    $data = curl_exec($ch);
+    curl_close($ch);
+    return $data;
 }
 
-  function showContent($input, $url) {
+function getContents($input, $url, $icon, $theUrl)
+{
     /* ===  Once city is typed in get the
-            contents and show it back to the user                                                     ==== */
-        if (!empty($input)) {
-            $urlContents = curl($url);
-            $weatherArray = json_decode($urlContents, true);
-            $weather = $input." currently has ".$weatherArray['weather'][0]['description'].".";
-            echo $weather;
-
-          }
-  }
-
-?>
+            contents and show it back to the user       ==== */
+    if (!empty($input)) {
+        $urlContents = curl($url);
+        $weatherArray = json_decode($urlContents, true);
+        $weather = $input." currently has ".$weatherArray['weather'][0]['description'].".";
+                $icon = $weatherArray['weather'][0]['icon'];
+                $newIconUrl = $theUrl.$icon.'.png';
+        // echo $weather;
+                echo '<img class="img img-responsive" src='.$newIconUrl.'  alt='.$input.'-'.'Weather >';
+    }
+}
