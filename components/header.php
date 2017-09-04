6047424eeda03caa90b5a7ec15a55a61a0814b34
Yannis Spyrou <js15js@gmail.com>
2017-09-04 21:43:59 +0100

    Add different icons

--- components/header.php
+++ components/header.php
@@ -10,7 +10,9 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css" integrity="sha384-y3tfxAZXuh4HwSYylfB+J125MxIs6mR5FOHamPBG064zB+AFeWH94NdvaCBm8qnd" crossorigin="anonymous">
-	<link rel="stylesheet" type="text/css" href="assets/css/styles.css">
+     <!-- Weather Icon Fonts -->
+		<link rel="stylesheet" href="assets/css/weather-icons.min.css">
+		<link rel="stylesheet" type="text/css" href="assets/css/styles.css">
 
 
 
--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 // 						Variables
 //    =========================================================
 $city = $_GET['city'];
-$error ="";
+$error ;
 $key ='__KEY__';
     
 /* === Get url ==== */
@@ -11,33 +11,58 @@ $urlWithCity = 'http://api.openweathermap.org/data/2.5/weather?q='.$city.$key;
 
 $imageUrl = 'http://openweathermap.org/img/w/'.$png;
     
+
 //    =========================================================
 // 						Functions
 //    =========================================================
 
-function curl($url)
-{
-    #  HTTP request
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-    $data = curl_exec($ch);
-    curl_close($ch);
-    return $data;
+function curl($url) {
+	#  HTTP request
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+	$data = curl_exec($ch);
+	curl_close($ch);
+	return $data;
 }
 
-function getContents($input, $url, $icon, $theUrl)
-{
+  function getContents($input, $url, $icon, $theUrl) {
     /* ===  Once city is typed in get the
             contents and show it back to the user       ==== */
-    if (!empty($input)) {
-        $urlContents = curl($url);
-        $weatherArray = json_decode($urlContents, true);
-        $weather = $input." currently has ".$weatherArray['weather'][0]['description'].".";
-                $icon = $weatherArray['weather'][0]['icon'];
-                $newIconUrl = $theUrl.$icon.'.png';
-        // echo $weather;
-                echo '<img class="img img-responsive" src='.$newIconUrl.'  alt='.$input.'-'.'Weather >';
-    }
-}
+        if (!empty($input)) {
+            $urlContents = curl($url);
+            $weatherArray = json_decode($urlContents, true);
+            $weather = $input." currently has ".$weatherArray['weather'][0]['description'].".";
+			$icon = $weatherArray['weather'][0]['icon']; // Access the icon in the json file
+			$newIconUrl = $theUrl.$icon.'.png'; // concatenate and inject to the img's src attribute
+			$wiClass;
+
+
+			 // Work In Progress : Use third party icons instead of the ones provided by the API
+			switch ($icon) {
+				case "01n":
+				$wiClass.="wi-owm-night-800";
+				break;
+
+				case "02n":
+				$wiClass.= 'wi-night-sleet';
+				break;
+
+				case "03n":
+				$wiClass.= 'wi-night';
+				break;
+				
+				case "04n":
+				$wiClass.= 'wi-night-sleet';
+				break;
+			default:
+			echo '<img class='.$newIconUrl.' src='.$newIconUrl.'  alt='.$input.'-'.'Weather >'; // Default option -> Openweather api icons
+			}
+           
+			echo'<i class='.$wiClass. "wi".' ></i>';
+
+          }          
+  }
+
+ ?>
